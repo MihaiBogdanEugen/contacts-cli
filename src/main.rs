e2338382142bfcaf21ee6480d32c3c427cbb7f1b
Bogdan Mihai <bogdanm@fb.com>
2023-03-15 13:56:19 +0100

    tests

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use core::panic;
 use regex::Regex;
 use std::collections::BTreeMap;
 
@@ -13,7 +12,12 @@ struct Contact {
 }
 
 trait ContactsService {
-    fn add(&mut self, name: String, phone_no_as_string: String, email: String) -> Result<(), String>;
+    fn add(
+        &mut self,
+        name: String,
+        phone_no_as_string: String,
+        email: String,
+    ) -> Result<(), String>;
 
     fn update_email(&mut self, name: &str, new_email: String) -> Result<(), String>;
 
@@ -27,7 +31,7 @@ trait ContactsService {
 }
 
 struct InMemoryContactsService {
-    contacts: BTreeMap<String, Contact>
+    contacts: BTreeMap<String, Contact>,
 }
 
 impl InMemoryContactsService {
@@ -39,8 +43,12 @@ impl InMemoryContactsService {
 }
 
 impl ContactsService for InMemoryContactsService {
-    
-    fn add(&mut self, name: String, phone_no_as_string: String, email: String) -> Result<(), String> {
+    fn add(
+        &mut self,
+        name: String,
+        phone_no_as_string: String,
+        email: String,
+    ) -> Result<(), String> {
         let de_phone_no_regex: Regex;
         match Regex::new(DE_PHONE_NO_REGEX) {
             Ok(r) => de_phone_no_regex = r,
@@ -149,54 +157,228 @@ impl ContactsService for InMemoryContactsService {
 
 fn main() {
     println!("Hello, world!");
+    let mut contacts_service: InMemoryContactsService = InMemoryContactsService::new();
+    contacts_service
+        .add(
+            "Bogdan".to_string(),
+            "491234567890".to_string(),
+            "bogdan@mail.com".to_string(),
+        )
+        .unwrap();
+    let contact = contacts_service.get("Bogdan").unwrap();
+    println!(
+        "Contact[{}, {}, {}]",
+        contact.name, contact.phone_no, contact.email
+    );
+}
 
-    let mut contacts: BTreeMap<String, Contact> = BTreeMap::new();
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    let name: String = String::from("Bogdan Mihai");
-    let phone_no_as_string: String = String::from("491234567890");
-    let email: String = String::from("bogdan.mihai@mail.com");
+    #[test]
+    fn test_in_memory_contacts_service_add_get() {
+        let mut contacts_service: InMemoryContactsService = InMemoryContactsService::new();
 
-    let phone_no;
-    match phone_no_as_string.parse::<u64>() {
-        Ok(val) => phone_no = val,
-        Err(_) => panic!("valid phone number is not a u64 value"),
+        let expected_name: String = "Bogdan".to_string();
+        let expected_phone_no_as_string: String = "491234567890".to_string();
+        let expected_email: String = "bogdan@mail.com".to_string();
+
+        contacts_service
+            .add(
+                expected_name.clone(),
+                expected_phone_no_as_string.clone(),
+                expected_email.clone(),
+            )
+            .unwrap();
+
+        let actual_contact: &Contact = contacts_service.get("Bogdan").unwrap();
+
+        assert_eq!(expected_name, actual_contact.name);
+        assert_eq!(
+            expected_phone_no_as_string,
+            actual_contact.phone_no.to_string()
+        );
+        assert_eq!(expected_email, actual_contact.email);
     }
 
-    let contact = Contact {
-        name: name.clone(),
-        phone_no,
-        email: email.clone(),
-    };
+    #[test]
+    fn test_in_memory_contacts_service_add_validations() {
+        let mut contacts_service: InMemoryContactsService = InMemoryContactsService::new();
 
-    contacts.insert(name.clone(), contact);
-    assert!(contacts.contains_key(&name));
+        let res_invalid_phone_no: Result<(), String> = contacts_service.add(
+            "valid name".to_string(),
+            "invalid phone no".to_string(),
+            "validemail@mail.com".to_string(),
+        );
+        assert!(res_invalid_phone_no.is_err());
 
-    match contacts.get(&name) {
-        Some(actual_contact) => {
-            assert_eq!(name, actual_contact.name);
-            assert_eq!(phone_no, actual_contact.phone_no);
-            assert_eq!(email, actual_contact.email);
-        }
-        None => panic!("there *must* be a contact with this name"),
+        let res_invalid_email: Result<(), String> = contacts_service.add(
+            "valid name".to_string(),
+            "491234567890".to_string(),
+            "invalid email".to_string(),
+        );
+        assert!(res_invalid_email.is_err());
     }
 
-    let email_regex: Regex;
-    match Regex::new(EMAIL_REGEX) {
-        Ok(r) => email_regex = r,
-        Err(_) => panic!("EMAIL_REGEX is not a valid regex"),
+    #[test]
+    fn test_in_memory_contacts_service_updates() {
+        let mut contacts_service: InMemoryContactsService = InMemoryContactsService::new();
+
+        let expected_name: String = "Bogdan".to_string();
+        let expected_phone_no_as_string: String = "491234567890".to_string();
+        let expected_email: String = "bogdan@mail.com".to_string();
+
+        contacts_service
+            .add(
+                expected_name.clone(),
+                expected_phone_no_as_string.clone(),
+                expected_email.clone(),
+            )
+            .unwrap();
+
+        let new_expected_phone_no_as_string: String = "490123456789".to_string();
+        contacts_service
+            .update_phone("Bogdan", new_expected_phone_no_as_string.clone())
+            .unwrap();
+
+        let new_email: String = "new_bogdan@mail.com".to_string();
+        contacts_service
+            .update_email("Bogdan", new_email.clone())
+            .unwrap();
+
+        let actual_contact: &Contact = contacts_service.get("Bogdan").unwrap();
+        assert_eq!(expected_name, actual_contact.name);
+        assert_eq!(
+            new_expected_phone_no_as_string,
+            actual_contact.phone_no.to_string()
+        );
+        assert_eq!(new_email, actual_contact.email);
     }
 
-    let de_phone_no_regex: Regex;
-    match Regex::new(DE_PHONE_NO_REGEX) {
-        Ok(r) => de_phone_no_regex = r,
-        Err(_) => panic!("DE_PHONE_NO_REGEX is not a valid regex"),
+    #[test]
+    fn test_in_memory_contacts_service_delete() {
+        let mut contacts_service: InMemoryContactsService = InMemoryContactsService::new();
+
+        let expected_name: String = "Bogdan".to_string();
+        let expected_phone_no_as_string: String = "491234567890".to_string();
+        let expected_email: String = "bogdan@mail.com".to_string();
+
+        contacts_service
+            .add(
+                expected_name.clone(),
+                expected_phone_no_as_string.clone(),
+                expected_email.clone(),
+            )
+            .unwrap();
+
+        contacts_service.delete("Bogdan").unwrap();
+
+        let res_get = contacts_service.get("Bogdan");
+        assert!(res_get.is_err());
     }
 
-    assert!(email_regex.is_match(&email));
-    assert!(!email_regex.is_match(&"invalid_email"));
+    #[test]
+    fn test_in_memory_contacts_service_list() {
+        let mut contacts_service: InMemoryContactsService = InMemoryContactsService::new();
+        contacts_service
+            .add(
+                "Eee".to_string(),
+                "491234567890".to_string(),
+                "mail@mail.com".to_string(),
+            )
+            .unwrap();
+        contacts_service
+            .add(
+                "Mmm".to_string(),
+                "491234567890".to_string(),
+                "mail@mail.com".to_string(),
+            )
+            .unwrap();
+        contacts_service
+            .add(
+                "Bbb".to_string(),
+                "491234567890".to_string(),
+                "mail@mail.com".to_string(),
+            )
+            .unwrap();
+        contacts_service
+            .add(
+                "Ddd".to_string(),
+                "491234567890".to_string(),
+                "mail@mail.com".to_string(),
+            )
+            .unwrap();
+        contacts_service
+            .add(
+                "Sss".to_string(),
+                "491234567890".to_string(),
+                "mail@mail.com".to_string(),
+            )
+            .unwrap();
+        contacts_service
+            .add(
+                "Aaa".to_string(),
+                "491234567890".to_string(),
+                "mail@mail.com".to_string(),
+            )
+            .unwrap();
+        contacts_service
+            .add(
+                "Eee".to_string(),
+                "491234567890".to_string(),
+                "mail@mail.com".to_string(),
+            )
+            .unwrap();
+        contacts_service
+            .add(
+                "Lll".to_string(),
+                "491234567890".to_string(),
+                "mail@mail.com".to_string(),
+            )
+            .unwrap();
+        contacts_service
+            .add(
+                "Ccc".to_string(),
+                "491234567890".to_string(),
+                "mail@mail.com".to_string(),
+            )
+            .unwrap();
+        contacts_service
+            .add(
+                "Aaa2".to_string(),
+                "491234567890".to_string(),
+                "mail@mail.com".to_string(),
+            )
+            .unwrap();
+        contacts_service
+            .add(
+                "Aaa3".to_string(),
+                "491234567890".to_string(),
+                "mail@mail.com".to_string(),
+            )
+            .unwrap();
 
-    assert!(de_phone_no_regex.is_match(&phone_no_as_string));
-    assert!(!de_phone_no_regex.is_match(&"481234567890"));
-    assert!(!de_phone_no_regex.is_match(&"49asda7890"));
-    assert!(!de_phone_no_regex.is_match(&"asdafsdgs"));
+        let page0: Vec<&Contact> = contacts_service.list(0, 3);
+        assert_eq!(3, page0.len());
+        assert_eq!("Aaa", page0.get(0).unwrap().name);
+        assert_eq!("Aaa2", page0.get(1).unwrap().name);
+        assert_eq!("Aaa3", page0.get(2).unwrap().name);
+
+        let page1: Vec<&Contact> = contacts_service.list(1, 3);
+        assert_eq!(3, page1.len());
+        assert_eq!("Bbb", page1.get(0).unwrap().name);
+        assert_eq!("Ccc", page1.get(1).unwrap().name);
+        assert_eq!("Ddd", page1.get(2).unwrap().name);
+
+        let page2: Vec<&Contact> = contacts_service.list(2, 3);
+        assert_eq!(3, page2.len());
+        assert_eq!("Eee", page2.get(0).unwrap().name);
+        assert_eq!("Lll", page2.get(1).unwrap().name);
+        assert_eq!("Mmm", page2.get(2).unwrap().name);
+
+        let page3: Vec<&Contact> = contacts_service.list(3, 3);
+        assert_eq!(1, page3.len());
+        assert_eq!("Sss", page3.get(0).unwrap().name);
+    }
 }
